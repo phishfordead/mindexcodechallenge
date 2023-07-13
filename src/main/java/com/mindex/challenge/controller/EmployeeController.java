3651d0c12df94a9b8a4843311e7f6fddca192c5a
Matt Bachman <mbachman@standardgroup.com>
2023-07-12 21:11:04 -0400

    moved compensation and reportingstructure cntrlers

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.mindex.challenge.controller;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.service.CompensationService;
 import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.service.ReportingStructureService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,12 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+
+    @Autowired
+    private CompensationService compensationService;
 
     @PostMapping("/employee")
     public Employee create(@RequestBody Employee employee) {
@@ -35,4 +46,42 @@ public class EmployeeController {
         employee.setEmployeeId(id);
         return employeeService.update(employee);
     }
+
+    // function: readReportingStructure
+    // parameters: id - string containing the Id of a specific employee
+    // returns: The ReportingStructure for a specific employee.
+    // description: REST endpoint for getting the ReportingStructure for a
+    //              specific employee based on it's *id*.
+    @GetMapping("/employee/reportingstructure/{id}")
+    public ReportingStructure readReportingStructure(@PathVariable String id) {
+        LOG.debug("Received ReportingStructure read request for employee id [{}]", id);
+        ReportingStructure rs = reportingStructureService.read(id);
+        return rs;
+    }
+
+    // function: createCompensation
+    // parameters: compensation - The Compensation object to be inserted
+    //                            into the database
+    // returns: The fully fleshed out Compensation object.
+    // description:  REST endpoint for inserting a *compensation* into the
+    //               database.
+    @PostMapping("/employee/compensation")
+    public Compensation createCompensation(@RequestBody Compensation compensation) {
+        LOG.debug("Received compensation create request for [{}]", compensation);
+        Compensation temp = compensationService.create(compensation);
+        temp.setEmployee(employeeService.read(temp.getEmployee().getEmployeeId()));
+        return temp;
+    }
+
+    // function: readCompensation
+    // parameters: id - string containing the Id of a specific employee
+    // returns: The Compensation object for the employee with the specific *id*.
+    @GetMapping("/employee/compensation/{id}")
+    public Compensation readCompensation(@PathVariable String id) {
+        LOG.debug("Received employee compensation read request for id [{}]", id);
+        Compensation temp = compensationService.read(id);
+        temp.setEmployee(employeeService.read(temp.getEmployee().getEmployeeId()));
+        return temp;
+    }
+    
 }
